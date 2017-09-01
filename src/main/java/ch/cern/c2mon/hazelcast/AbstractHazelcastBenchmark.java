95e9908cd894673af9917d06f9fd6494a0ea0174
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-01 17:38:59 +0200

    Create get benchmark, set up base for abstract class

--- src/main/java/ch/cern/c2mon/hazelcast/AbstractHazelcastBenchmark.java
+++ src/main/java/ch/cern/c2mon/hazelcast/AbstractHazelcastBenchmark.java
@@ -5,9 +5,13 @@ import org.yardstickframework.BenchmarkDriverAdapter;
 
 import java.util.Map;
 
+import javax.cache.Cache;
+
 public abstract class AbstractHazelcastBenchmark extends BenchmarkDriverAdapter {
 
     private String cacheName;
+
+    protected Cache<Long, String> cache;
 
     public AbstractHazelcastBenchmark(String cacheName) {
         this.cacheName = cacheName;
@@ -16,7 +20,6 @@ public abstract class AbstractHazelcastBenchmark extends BenchmarkDriverAdapter 
     @Override
     public void setUp(BenchmarkConfiguration cfg) throws Exception {
         super.setUp(cfg);
-
     }
 
     @Override
---
+++ src/main/java/ch/cern/c2mon/hazelcast/benchmarks/HazelcastGetBenchmark.java
@@ -0,0 +1,24 @@
+package ch.cern.c2mon.hazelcast.benchmarks;
+
+import java.util.Map;
+
+import ch.cern.c2mon.hazelcast.AbstractHazelcastBenchmark;
+
+/**
+ * @author Szymon Halastra
+ */
+public class HazelcastGetBenchmark extends AbstractHazelcastBenchmark {
+
+  public HazelcastGetBenchmark(String cacheName) {
+    super("tagCache");
+  }
+
+  @Override
+  public boolean test(Map<Object, Object> map) throws Exception {
+    int key = nextRandom(args.range());
+
+    cache.get(key);
+
+    return true;
+  }
+}
