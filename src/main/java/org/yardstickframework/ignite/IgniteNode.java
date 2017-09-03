f1b838145b5b5b530efcac1254963526ed2fdf5b
Szymon Halastra <szymon.edward.halastra@cern.ch>
2017-09-03 17:19:08 +0200

    Set up Ignite benchmark server

--- src/main/java/org/yardstickframework/ignite/IgniteNode.java
+++ src/main/java/org/yardstickframework/ignite/IgniteNode.java
@@ -1,24 +1,57 @@
 package org.yardstickframework.ignite;
 
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
 import org.yardstickframework.BenchmarkConfiguration;
 import org.yardstickframework.BenchmarkServer;
+import org.yardstickframework.BenchmarkUtils;
+import org.yardstickframework.common.NodeType;
+
+import static org.yardstickframework.BenchmarkUtils.jcommander;
 
 /**
  * @author Szymon Halastra
  */
 public class IgniteNode implements BenchmarkServer {
+
+  private Ignite ignite;
+
+  private NodeType nodeType = NodeType.SERVER;
+
+  public IgniteNode() {
+  }
+
+  public IgniteNode(NodeType nodeType) {
+    this.nodeType = nodeType;
+  }
+
+  public IgniteNode(NodeType nodeType, Ignite ignite) {
+    this.nodeType = nodeType;
+    this.ignite = ignite;
+  }
+
   @Override
   public void start(BenchmarkConfiguration cfg) throws Exception {
+    IgniteArguments args = new IgniteArguments();
 
+    jcommander(cfg.commandLineArguments(), args, "<ignite-node>");
+
+    switch (nodeType) {
+      case CLIENT:
+        break;
+      case SERVER:
+      case LITE_MEMBER:
+        break;
+    }
   }
 
   @Override
   public void stop() throws Exception {
-
+    Ignition.stopAll(true);
   }
 
   @Override
   public String usage() {
-    return null;
+    return BenchmarkUtils.usage(new IgniteArguments());
   }
 }
