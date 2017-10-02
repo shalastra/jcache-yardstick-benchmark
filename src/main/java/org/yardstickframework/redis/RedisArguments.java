745fe3d7f55b221a46a03052f0f8d1f3e1f6151b
Szymon <szymon.halastra@gmail.com>
2017-10-02 20:24:19 +0200

    Configure Redisson cluster

--- src/main/java/org/yardstickframework/redis/RedisArguments.java
+++ src/main/java/org/yardstickframework/redis/RedisArguments.java
@@ -1,5 +1,6 @@
 package org.yardstickframework.redis;
 
+import com.beust.jcommander.Parameter;
 import org.yardstickframework.common.Arguments;
 
 /**
@@ -7,5 +8,22 @@ import org.yardstickframework.common.Arguments;
  */
 public class RedisArguments extends Arguments {
 
+  /** */
+  @Parameter(names = {"-cfg", "--Config"}, description = "Configuration file")
+  private String cfg = "config/redisson/redisson-config.xml";
 
+  /**
+   * @return Configuration file.
+   */
+  public String configuration() {
+    return cfg;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("RedisArguments{");
+    sb.append("cfg='").append(cfg).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
 }
